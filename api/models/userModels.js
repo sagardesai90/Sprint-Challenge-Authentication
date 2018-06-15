0754fd54dae218d906b507d07deeceb23b29e64b
sagardesai90 <saagardesai@gmail.com>
2018-06-15 13:41:21 -0500

    have to get the get to work

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -42,7 +42,10 @@ UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
-  return bcrypt.compare(plainTextPW, callBack);
+  bcrypt.compare(plainTextPW, this.password, function(err, matched){
+    if (err) return callBack(err)
+    else return callBack(null, matched)
+  });
 };
 
 module.exports = mongoose.model('User', UserSchema);
--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/auth-users', {
-  useMongoClient: true
-});
+mongoose.connect('mongodb://localhost/auth-users')
+  .then(() => {
+    console.log('\n***Connected to db***\n');
+  });
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
