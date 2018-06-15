e7a57907b8152af47ba55a217b912d3fcebb5053
sagardesai90 <saagardesai@gmail.com>
2018-06-15 13:40:42 -0500

    almost done with core sprint

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,6 +5,10 @@ const createUser = (req, res) => {
   const { username, password } = req.body;
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  User
+    .create({username, password})
+    .then(response => res.status(201).json({data: response}))
+    .catch(err => res.status(500).json({message: err}))
 };
 
 module.exports = {
